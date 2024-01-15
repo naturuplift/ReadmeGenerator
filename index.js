43ea28e718082194de738d2fc1444d451689b299
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-01-14 23:44:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+function generateREADME(answers) {
+    // Define the README content template
+    const questions = `
+  # ${answers.title}
+  
+  ## Description
+  ${answers.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${answers.installation}
+  
+  ## Usage
+  ${answers.usage}
+  
+  ## License
+  ![License Badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+  
+  This project is licensed under the ${answers.license} license.
+  
+  ## Contributing
+  ${answers.contributing}
+  
+  ## Tests
+  ${answers.tests}
+  
+  ## Questions
+  For questions about this project, please contact [${answers.username}](https://github.com/${answers.username}) at ${answers.email}.
+  `;
+  
+    return questions;
+  }
+
+console.log(questions);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
